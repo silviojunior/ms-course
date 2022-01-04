439e0d846f7e48d10ede56bf41477f5f49fcac16
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2022-01-04 17:36:32 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TB_WORKER (name, daily_income) values ('Bob', 200.0);
+insert into TB_WORKER (name, daily_income) values ('Maria', 300.0);
+insert into TB_WORKER (name, daily_income) values ('Alex', 250.0);
