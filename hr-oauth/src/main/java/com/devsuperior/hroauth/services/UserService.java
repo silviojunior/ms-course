3ab1415a23a9a147ff2872a9bad4e003a766e1cd
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2022-01-10 12:24:12 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private  static Logger logger = LoggerFactory.getLogger((UserService.class));
 
@@ -21,6 +24,18 @@ public class UserService {
         if(user == null){
             logger.error("Email not found: " + email);
             throw new IllegalArgumentException("Email not found");
+        }
+
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userFeignClient.findByEmail(username).getBody();
+        if(user == null){
+            logger.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
         }
 
         return user;
