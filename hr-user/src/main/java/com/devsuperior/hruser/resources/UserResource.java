0ac2e21c1f97fabee5cf054c3e78c53ed6597eaa
Silvio Ferreira de Souza Junior <silvio.junior@orsegups.com.br>
2022-01-10 09:24:35 -0300

    hr-oauth created

--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -1,2 +1,34 @@
-package com.devsuperior.hruser.resources;public class UserResource {
+package com.devsuperior.hruser.resources;
+
+import com.devsuperior.hruser.entities.User;
+import com.devsuperior.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    private static Logger logger = LoggerFactory.getLogger(UserResource.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value="/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email){
+
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
 }
